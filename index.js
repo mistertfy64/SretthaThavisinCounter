52b7d0ecaeca35d2256ef883028b835166d5f669
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-09-06 16:24:58 +0700

    change: add faces

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const START_TIME = 1692637200 * 1000;
 const DAY_TIME = 86400000;
+const FACE_INTERVALS = [336, 28, 7, 1];
 const loop = setInterval(() => {
 	const now = Date.now();
 	const days = Math.floor((now - START_TIME) / DAY_TIME);
 	const counter = document.getElementById("counter");
 	counter.innerText = days;
 }, 1000);
+
+function createFaces(days) {
+	const facesHTML = document.getElementById("faces");
+	let faceIntervalIndex = 0;
+	while (days > 0 && faceIntervalIndex < 4) {
+		threshold = FACE_INTERVALS[faceIntervalIndex];
+		if (days >= threshold) {
+			days -= threshold;
+			let face = document.createElement("img");
+			face.src = `./public/tpmc-srettha-${threshold}.png`;
+			facesHTML.appendChild(face);
+		} else {
+			faceIntervalIndex++;
+		}
+	}
+}
+
+createFaces(Math.floor((Date.now() - START_TIME) / DAY_TIME));
